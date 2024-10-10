c0e08669cc3ff8808eb68e77c38d32a89b6d4d25
buungoo <alxber-0@student.ltu.se>
2024-10-10 21:01:54 +0200

    CylinderVSensValve changed

--- HMI/CylinderVSensValve/CylinderVSensValve_sDefault.cnv.cs
+++ HMI/CylinderVSensValve/CylinderVSensValve_sDefault.cnv.cs
@@ -23,6 +23,13 @@ namespace HMI.Main.Symbols.CylinderVSensValve
 			// The InitializeComponent() call is required for Windows Forms designer support.
 			//
 			InitializeComponent();
+			this.REQ_Fired += REQ_Fired_EventHandler;
+		}
+
+		void REQ_Fired_EventHandler(object sender, REQEventArgs e)
+		{
+			bool output = e.FWD_IN.Value;
+			this.FireEvent_CNF(output);
 		}
 	}
 }
